b0370523c6b5b7c60588c0661286741e8381bca9
Yeferson Nova <yeferson_nova@outlook.com>
2025-03-04 02:05:31 -0500

    Initial commit

---
+++ src/test/java/com/ynova/hotel_service/HotelServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ynova.hotel_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
